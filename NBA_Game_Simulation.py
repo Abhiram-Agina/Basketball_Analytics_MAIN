dad4cf1e8f04709c3a2fa6fef77dd1552f711ba4
Abhiram-Agina <50762306+Abhiram-Agina@users.noreply.github.com>
2022-01-06 20:51:33 -0800

    Update NBA_Game_Simulation.py

--- NBA_Game_Simulation.py
+++ NBA_Game_Simulation.py
@@ -2,9 +2,13 @@ import pandas as pd
 import random as rnd
 import matplotlib.pyplot as plt
 import streamlit as st
+from PIL import Image
 
 st.markdown("[Return to HomePage](https://share.streamlit.io/abhiram-agina/basketball_analytics_main/main/Basketball_Analytics_MAIN.py)")
 st.title("Predicting Game Results")
+
+image = Image.open('PICS/PredictingGames.jpg')
+
 st.title("*Warriors vs. Rockets*")
 
 gamesDF = pd.read_csv('DATA/nba.games.stats.csv')
